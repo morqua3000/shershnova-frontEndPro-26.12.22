76436424fa7aa089d764ddbaece90516838c9a85
morqua <mariasersneva@gmail.com>
2023-01-16 20:50:42 +0200

    Update index.js

--- 15hw/index.js
+++ 15hw/index.js
@@ -1,8 +1,12 @@
+alert("allo");
 let a = prompt("Enter number");
 let b = prompt("Enter degree");
 function powpow(x,y){
-    let ret = Math.pow(x,y);
-    return ret;
+    if (y!=1){
+        return x*=powpow(x,y-1);
+    }else{
+        return x;
+    }
 }
 let c = powpow(a,b);
 document.write(a + " in " + b + "th degree is " + c);
